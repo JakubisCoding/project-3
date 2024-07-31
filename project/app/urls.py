5452671d9bd29e0be6920461bf112a0a1e835471
Jakub Wiszowaty <Jakub060386@gmail.com>
2024-07-31 14:30:27 +0200

    project,app,urls,settings

---
+++ project/app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+
+urlpatterns = [
+    # path('app/user/create/', UserCreateView.as_view(), name='user_create'),
+    # path('app/login/', LoginView.as_view(), name='login'),
+    # path('app/logout/', LogoutView.as_view(), name='logout'),
+    # path('app/task/create/', TaskCreateView.as_view(), name='task_create'),
+    # path('app/tasks-created-by-user/', TasksCreatedByUser.as_view(), name='user_tasks'),
+    # path('app/user-tasks-stats/', UserTasksStatsAPIView.as_view(), name='user-tasks-stats'),
+    # path('app/unassigned-tasks/', UnassignedTasksAPIView.as_view(), name='unassigned-tasks'),
+    # path('app/task/executor/', TaskWithExecutorAPIView.as_view(), name='task_executor'),
+    # path('app/user-tasks/', UserTasksAPIView.as_view(), name='my-tasks'),
+    # path('app/become-executor/<int:task_id>/', BecomeExecutorAPIView.as_view(), name='become-executor'),
+    # path('app/mark-task-done/<int:task_id>/', MarkTaskDoneAPIView.as_view(), name='mark-task-done'),
+    # path('app/clear_db/', ClearDatabaseView.as_view(), name='clear_db'),
+]
